f83a8de11f5a0acf458ce7a42f55b909d8a17fcb
Hailey <87118337+Heewon-Seo@users.noreply.github.com>
2022-05-01 16:28:40 +0900

    Time: 1 ms (96.07%), Space: 42.4 MB (83.73%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int answer = 0;
+        
+        
+        for(int i = 0; i < nums.length ; i++) {
+            answer ^= nums[i];
+        }
+        
+        return answer;
+        
+    }
+}
